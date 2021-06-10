dfca1bf4a701e5b6f38503fdd8e925b3ea1eb65f
Andrew Bashore <andrew@bashtech.net>
2021-06-10 11:00:26 -0400

    Support for Helix Emotes

--- chat.go
+++ chat.go
@@ -30,6 +30,45 @@ type BadgeVersion struct {
 	ImageUrl4x string `json:"image_url_4x"`
 }
 
+// GetChatEmoteParams ...
+type GetChatEmoteParams struct {
+	BroadcasterID string `query:"broadcaster_id"`
+}
+
+// GetChatSetEmoteParams ...
+type GetChatSetEmoteParams struct {
+	EmoteSetID string `query:"emote_set_id"`
+}
+
+// GetChatEmoteResponse ...
+type GetChatEmoteResponse struct {
+	ResponseCommon
+	Data ManyChatEmote
+}
+
+// ManyChatEmote ...
+type ManyChatEmote struct {
+	Emotes []ChatEmote `json:"data"`
+}
+
+// ChatEmote ...
+type ChatEmote struct {
+	ID         string         `json:"id"`
+	Name       string         `json:"name"`
+	Versions   []EmoteImages  `json:"images"`
+	Tier       string         `json:"tier"`
+	EmoteType  string         `json:"emote_type"`
+	EmoteSetID string         `json:"emote_set_id"`
+}
+
+// BadgeVersion ...
+type EmoteImages struct {
+	Url1x string `json:"url_1x"`
+	Url2x string `json:"url_2x"`
+	Url4x string `json:"url_4x"`
+}
+
+
 // GetChannelChatBadges ...
 func (c *Client) GetChannelChatBadges(params *GetChatBadgeParams) (*GetChatBadgeResponse, error) {
 	resp, err := c.get("/chat/badges", &ManyChatBadge{}, params)
@@ -54,6 +93,48 @@ func (c *Client) GetGlobalChatBadges() (*GetChatBadgeResponse, error) {
 	channels := &GetChatBadgeResponse{}
 	resp.HydrateResponseCommon(&channels.ResponseCommon)
 	channels.Data.Badges = resp.Data.(*ManyChatBadge).Badges
+
+	return channels, nil
+}
+
+// GetGlobalChatEmotes ...
+func (c *Client) GetGlobalChatEmotes() (*GetChatEmoteResponse, error) {
+	resp, err := c.get("/chat/emotes/global", &ManyChatEmote{}, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	channels := &GetChatEmoteResponse{}
+	resp.HydrateResponseCommon(&channels.ResponseCommon)
+	channels.Data.Emotes = resp.Data.(*ManyChatEmote).Emotes
+
+	return channels, nil
+}
+
+// GetChannelChatEmotes ...
+func (c *Client) GetChannelChatEmotes(params *GetChatEmoteParams) (*GetChatEmoteResponse, error) {
+	resp, err := c.get("/chat/emotes", &ManyChatEmote{}, params)
+	if err != nil {
+		return nil, err
+	}
+
+	channels := &GetChatEmoteResponse{}
+	resp.HydrateResponseCommon(&channels.ResponseCommon)
+	channels.Data.Emotes = resp.Data.(*ManyChatEmote).Emotes
+
+	return channels, nil
+}
+
+// GetSetChatEmotes ...
+func (c *Client) GetSetChatEmotes(params *GetChatSetEmoteParams) (*GetChatEmoteResponse, error) {
+	resp, err := c.get("/chat/emotes/set", &ManyChatEmote{}, params)
+	if err != nil {
+		return nil, err
+	}
+
+	channels := &GetChatEmoteResponse{}
+	resp.HydrateResponseCommon(&channels.ResponseCommon)
+	channels.Data.Emotes = resp.Data.(*ManyChatEmote).Emotes
 
 	return channels, nil
 }
